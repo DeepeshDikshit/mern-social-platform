8ce4f7aa45c8a30fc93b15670bf8506a2dfe8e10
Deepesh Sharma <152675563+DeepeshDikshit@users.noreply.github.com>
2026-05-02 12:44:59 +0530

    Update AI model version in ai.service.js

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -22,7 +22,7 @@ export async function generateCaption(file) {
         { text: "Caption this image." },
     ];
     const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
+        model: "gemini-2.5-flash-lite",
         contents: contents,
         config:{
             systemInstruction:`
